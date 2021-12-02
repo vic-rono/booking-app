82dd3a94ba58204975288b5e049712e0d890430b
Victor Rono <61822296+vic-rono@users.noreply.github.com>
2021-12-02 15:45:35 +0300

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -74,7 +74,7 @@ const Register = () => {
               }}
             />
             <input
-              type="text"
+              type="password"
               className="form-control"
               placeholder="password"
               value={password}
@@ -83,7 +83,7 @@ const Register = () => {
               }}
             />
             <input
-              type="text"
+              type="password"
               className="form-control"
               placeholder="confirm-password"
               value={confirmpassword}
